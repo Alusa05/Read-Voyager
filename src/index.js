e87830763a8a5846cec8b3b8ab9989b22780b6c2
Lisa Alusa <sayilisaalusa@gmail.com>
2025-03-26 08:44:33 +0300

    added the event listeners

--- src/index.js
+++ src/index.js
@@ -7,3 +7,45 @@ const bookList = document.createElement('div');
     let currentView = 'home';
     let books = [];
     let favorites = [];
+
+    document.addEventListener('DOMContentLoaded', () => {
+        fetchBooks();
+        setupNavigation();
+
+    });
+
+    //Fetching the books from db.json
+    async function fetchBooks() {
+        try{
+            const response = await fetch('http://localhost:3000/books');
+            books = await response.json();
+            if (currentView === 'home') displayBooks(books);
+        } catch (error) {
+ 
+            console.error('Error fetching books', error);
+            bookList.innerHTML = `<p class="error">Failed to load books. Please try again later</p>`;
+        }
+        
+    }
+
+    // Displaying the books
+function displayBooks(booksToDisplay) {
+    bookList.innerHTML = booksToDisplay.map(book => `
+        <div class="book-card" data-id="${book.id}">
+            <img src="${book.cover}" alt="${book.title}" onerror="this.src='https://via.placeholder.com/150x200?text=No+Cover'">
+            <h3>${book.title}</h3>
+            <p>${book.author}</p>
+            <button class="favorite-btn"> Add to Favorites</button>
+        </div>
+    `).join('');
+
+    //Adding the event listeners to the favorite button
+    document.querySelectorAll(('.favorite-btn')).forEach(button => {
+        button.addEventListener('click', (e) => {
+            const bookId = parseInt(e.target.closest('.book-card').dataset.id);
+            addToFavorites(bookId);
+        });
+        
+    });
+
+    }
